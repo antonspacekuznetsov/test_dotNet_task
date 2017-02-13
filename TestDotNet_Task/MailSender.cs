381d0b6978873ec8457ced9f7284aa12dd8499aa
unknown <as.ky@ya.ru>
2017-02-13 07:17:39 +0300

    first commit now working version

---
+++ TestDotNet_Task/MailSender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Mail;
+
+namespace TestDotNet_Task
+{
+    //the class send mail then occure error in programm, for example occure exception
+
+    class MailSender:ISmtpSender
+    {
+        private SmtpClient _client;
+        private MailMessage _mail;
+        private int _allow;
+        public MailSender()
+        {
+            int port;
+            _mail = new MailMessage(ConfigurationManager.AppSettings["MailFrom"], ConfigurationManager.AppSettings["MailTo"], ConfigurationManager.AppSettings["MailSubject"], ConfigurationManager.AppSettings["MailBody"]);
+            _client = new SmtpClient(ConfigurationManager.AppSettings["MailServer"]);
+            int.TryParse(ConfigurationManager.AppSettings["MailPort"], out port);
+            _client.Port = port;
+            int.TryParse(ConfigurationManager.AppSettings["Allow"], out _allow);
+        }
+
+        public void SendMail(string message)
+        {
+
+            if(_allow==1)
+                try
+                {
+                    _mail.Body = message;
+                    _client.Send(_mail);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("+ERROR+" + e.Message);
+                }
+        }
+    }
+}
